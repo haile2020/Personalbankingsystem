2ccb18736ba5115caa346a5aa8a3ec8f55f6be31
haile2020 <aryamhaile2019@gmail.com>
2021-12-03 20:13:47 -0500

    Initial Commit

---
+++ PersonalBankingSystem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PersonalBankingSystem
+//
+//  Created by Haile on 12/3/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
